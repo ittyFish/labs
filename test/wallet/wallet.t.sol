9b6b44b5977e91f415d93c64e3cc883ed565706d
ittyFish <150668031+ittyFish@users.noreply.github.com>
2024-03-21 09:56:34 +0200

    Update wallet.t.sol

--- test/wallet/wallet.t.sol
+++ test/wallet/wallet.t.sol
@@ -3,18 +3,15 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "@hack/wallet/wallet.sol";
+import "@hack/gabaim.sol";
 
 
 contract WalletTest is Test {
-    Wallet public w;
+    Wallet1 public w;
 
     // Everything I need to start my test
     function setUp() public {
-        w = new Wallet();
-    }
-
-    function testDeposit() public {
+        w = new Wallet1();
     }
 
     function testWithdraw() public {
